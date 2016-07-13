826b2cb664caac3c7a2d045213de53dec7894b27
franta <frantisek.polach@gmail.com>
2016-07-13 11:29:12 +0200

    lib moved to btc pool

--- poolsrv/epoll.py
+++
@@ -1,6 +0,0 @@
-try:
-    from twisted.internet import epollreactor
-    epollreactor.install()
-except ImportError:
-    raise Exception("Failed to install epoll reactor")
-
--- poolsrv/posix_time.py
+++
@@ -1,7 +0,0 @@
-
-from time import time as posix_time
-
-def posix_secs():
-    return int(posix_time())
-
-__all__ = ['posix_time', 'posix_secs']
